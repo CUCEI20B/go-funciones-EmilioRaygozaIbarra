4811848f1309f1bdd9608c5bdb61b12d2e3c83d5
EmilioRaygozaIbarra <emilio.raygoza@alumnos.udg.mx>
2020-10-16 12:12:41 -0400

    Funcion Burbuja

--- burbuja.go
+++ burbuja.go
@@ -1,7 +1,18 @@
 package main
 
 func Burbuja(s []int64)  {
-
+	var aux int
+  l := len(arreglo)
+  for i:=0;i<l;i++{
+    for j:=0;j<l-1;j++{
+      if arreglo[j] > arreglo[j+1]{
+        aux = arreglo[j]
+        arreglo[j]=arreglo[j+1]
+        arreglo[j+1]=aux
+      }
+    }
+  }
+  fmt.Println(arreglo)
 }
 
 func main()  {
